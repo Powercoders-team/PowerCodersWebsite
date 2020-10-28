2302217aaff46bad52003a5ab98e8292e1ec8534
Powercoders-team <kevinliucuber@gmail.com>
2020-10-28 20:20:23 +1100

    chat: check whitespace

--- script-chat.js
+++ script-chat.js
@@ -4,6 +4,10 @@ document.getElementById('send').onclick = function(){
     let textbox = document.getElementById("textbox");
     let namebox = document.getElementById("namebox");
     let newMessage = document.createElement("li");
+    if(/^\s*$/.test(textbox.value)){
+        alert('Message cannot be empty.');
+        return;
+    }
     newMessage.innerHTML = '<p>' + (namebox.value + ': ' + textbox.value).replace(/</g, '&lt;').replace(/>/g, '&gt;') + '</p>';
     newMessage.className = 'chat-message';
     messages.appendChild(newMessage);
--- script-login.js
+++ script-login.js
@@ -4,14 +4,18 @@ document.getElementById('change').onclick = function(){
         Username <input type='text' placeholder='Username'><br>
         Password <input type='password' placeholder='Password'><br>
         Password (confirm) <input type='password' placeholder='Password Again'><br>
-        <button>Login</button>`;
+        <button id='click'>Sign up</button>`;
         this.innerHTML = 'Have an account?';
     }
     else{
         document.getElementById('login-box').innerHTML = `<h2>Login</h2>
         Username <input type='text' placeholder='Username'><br>
         Password <input type='password' placeholder='Password'><br>
-        <button>Login</button>`;
+        <button id='click'>Login</button>`;
         this.innerHTML = 'Don\'t have an account?';
     }
+};
+
+document.getElementById('click').onclick = function(){
+    
 };
