5f4cc33586b033e68a062f9a48ce06bfe62f6bb0
Powercoders-team <kevinliucuber@gmail.com>
2020-11-29 11:08:25 +1100

    tab: fix tab system

--- script-tab.js
+++ script-tab.js
@@ -1,5 +1,10 @@
-// var tabs = document.getElementsByClassName('tab');
-// for(let i = 0; i < tabs.length; i++){
-
-// }
-// TODO: finish tab
+var tabs = document.getElementsByClassName('tab');
+for(let i = 0; i < tabs.length; i++){
+    let children = tabs[i].children;
+    for(let j = 0; j < children.length - 1; j++){
+        children[j].onclick = function(){
+            'use strict';
+            tabs[i].lastElementChild.textContent = eval(this.dataset.message);
+        }
+    }
+}
